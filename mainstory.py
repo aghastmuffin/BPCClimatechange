5ea4ab8d650f40830f92790c8e8d12f4583b0a8e
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2021-06-03 16:02:08 -0700

    Update mainstory.py

--- mainstory.py
+++ mainstory.py
@@ -8,3 +8,9 @@ except:
     print('A required module (try pygame) was not available. Please pip this module in. \n Thanks!')
 print('Done')
 messagebox.showerror('Disclaimer', 'Disclaimer: \n I didn\'t have a lot of time to work on this, and pygame is hard to use')
+#10 x 10 screen
+#amescreen = ['##########', '##########', '##########', '##########', '##########', '##########', '##########', '##########', '##########', '##########']
+#add g. for some reason github is glitching
+#for item in gamescreen:
+  print(item)
+  print('{}'.format(item))
