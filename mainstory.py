a99c9be4224328d001260e5cec538f7f8840fdbb
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2021-06-03 11:36:41 -0700

    Add files via upload

---
+++ mainstory.py
@@ -0,0 +1,8 @@
+try:
+    import pygame
+    try:
+        import tkinter
+    except:
+        print('A required module (try tkinter) was not available. Please pip this module in. \n Thanks!')
+except:
+    print('A required module (try pygame) was not available. Please pip this module in. \n Thanks!')
